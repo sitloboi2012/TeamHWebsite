db5ab796b387eb2295ef711c2f67213ff5d3266b
snowman72 <s3978681@rmit.edu.vn>
2023-05-18 11:05:01 +0700

    Add node.js to addproductvendor page
    - Form data can be submitted
    - Some changes in page appearance

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const multer = require('multer');
+const upload = multer({ dest: "public/images" });
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+const Product = require('./model/Product');
+
+app.post("/form-processing", upload.single("myFile"), (req, res) => {
+    // Stuff to be added later
+    console.log(req.file);
+    console.log(req.body.product_name);
+    console.log(req.body.category);
+    console.log(req.body.price);
+    res.send("upload success");
+});
+
+app.get("/add-product-vendor", (req, res) => {
+    res.render('addproductvendor');
+});
+
+app.post("/processing", upload.single("myFile"), (req, res) => {
+    // Stuff to be added later
+    console.log(req.file);
+    console.log(req.body.product_name);
+    console.log(req.body.category);
+    console.log(req.body.price);
+    console.log(req.body.description);
+    res.send("upload success");
+});
+
+app.get('/add-product', (req, res) => {
+    res.render('addProduct');
+})
+
+app.get('/customer-page', (req, res) => {
+    res.render('customerPage');
+})
+
+app.post('/processing', (req, res) => {
+    console.log(req.body.product_name);
+    res.send('Congratulations');
+})
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
